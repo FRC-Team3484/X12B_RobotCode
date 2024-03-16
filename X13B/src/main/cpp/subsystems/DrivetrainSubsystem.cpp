49e54973fe4cadd0507ec19ac70e8e6a00ea00c4
michamichaa <116735133+michamichaa@users.noreply.github.com>
2024-03-16 19:12:21 -0400

    Added Brake Mode

--- X13B/src/main/cpp/subsystems/DrivetrainSubsystem.cpp
+++ X13B/src/main/cpp/subsystems/DrivetrainSubsystem.cpp
@@ -7,6 +7,8 @@ DrivetrainSubsystem::DrivetrainSubsystem() {
     _motor_right.ConfigFactoryDefault(); //Resets the Motor
     _motor_left.SetInverted(!RIGHT_INVERTED);
     _motor_right.SetInverted(RIGHT_INVERTED);
+    _motor_left.SetNeutralMode(ctre::phoenix::motorcontrol::Brake);
+    _motor_right.SetNeutralMode(ctre::phoenix::motorcontrol::Brake);
 }
 
 void DrivetrainSubsystem::Periodic() {}
